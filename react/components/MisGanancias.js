12855aca96efff9304931d750515895bad4ce986
Carlos <narvaezcarlos@live.com>
2023-04-25 11:22:25 -0500

    Datos de sesion

--- react/components/MisGanancias.js
+++ react/components/MisGanancias.js
@@ -1,4 +1,15 @@
+import React, { useEffect } from 'react';
+import { useRenderSession } from 'vtex.session-client';
+
 const MisGanancias = () => {
+	const { loading, session, error } = useRenderSession();
+
+	useEffect(() => {
+		console.log({ session });
+		console.log(session?.namespaces.authentication.storeUserEmail.value);
+		console.log(session?.namespaces.authentication.storeUserId.value);
+	}, [session]);
+
 	return (
 		<div>
 			<h1>Mis Ganancias</h1>
