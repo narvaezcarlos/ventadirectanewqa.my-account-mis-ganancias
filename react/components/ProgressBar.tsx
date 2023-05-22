5a97443bc4122eefebbfb4b96d049a1348cebac6
narvaezcarlos <narvaezcarlos@live.com>
2023-05-22 08:28:29 -0500

    Actualizacion ambiente prod

--- react/components/ProgressBar.tsx
+++ react/components/ProgressBar.tsx
@@ -21,8 +21,8 @@ const ProgressBar: React.FC<ProgressBarProps> = ({ userId }) => {
     } else {
       try {
         const response = await fetch(
-          `https://websvrx.hermeco.com/offcorsspersonalization/public/api/Ventadirectanew/getUserByUserId/${userId}`
-        );
+					`https://websvrx.hermeco.com/offcorsspersonalization/public/api/linkapp/getUserByUserId/${userId}`
+				);
         const responseData = await response.json();
         if (response.ok) {
           setUser(responseData);
--- react/hooks/validatelevel.service.ts
+++ react/hooks/validatelevel.service.ts
@@ -5,7 +5,7 @@ export const useVerifyUserId = (userId: string) => {
  const baseURL = 'https://websvrx.hermeco.com/offcorsspersonalization/public/'
  
   useEffect(() => {
-      fetch(`${baseURL}api/Ventadirectanew/getNiveles`)
+      fetch(`${baseURL}api/linkapp/getNiveles`)
 				.then((response) => {
 					console.log('responsedata', response);
 				})
