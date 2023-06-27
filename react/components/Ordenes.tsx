6bb3264272e12b5bdce34ccfebe3a9a640242eae
Erixk90 <erickleonesreyes@gmail.com>
2023-06-26 21:58:33 -0500

    fix: formating ordervalue

--- react/components/Ordenes.tsx
+++ react/components/Ordenes.tsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import * as Intl from 'intl';
 import styles from './ordenes.css'
+import numbro from 'numbro';
+import  '../numbro-lan-config';
 
 interface Order {
   orderId: number;
@@ -14,6 +16,7 @@ interface Props {
   idLinker: string;
 }
 
+numbro.setLanguage('es-EC');
 const Ordenes = ({ idLinker }: Props) => {
   const [orders, setOrders] = useState<Order[]>([]);
   const [filteredOrders, setFilteredOrders] = useState<Order[]>([]);
@@ -73,7 +76,6 @@ const Ordenes = ({ idLinker }: Props) => {
   const svg = <svg width="24" height="28" viewBox="0 0 24 28" fill="none" xmlns="http://www.w3.org/2000/svg">
   <path d="M21.3333 3.33335H20V0.666687H17.3333V3.33335H6.66667V0.666687H4V3.33335H2.66667C1.18667 3.33335 0.0133333 4.53335 0.0133333 6.00002L0 24.6667C0 26.1334 1.18667 27.3334 2.66667 27.3334H21.3333C22.8 27.3334 24 26.1334 24 24.6667V6.00002C24 4.53335 22.8 3.33335 21.3333 3.33335ZM21.3333 24.6667H2.66667V11.3334H21.3333V24.6667ZM21.3333 8.66669H2.66667V6.00002H21.3333V8.66669ZM8 16.6667H5.33333V14H8V16.6667ZM13.3333 16.6667H10.6667V14H13.3333V16.6667ZM18.6667 16.6667H16V14H18.6667V16.6667ZM8 22H5.33333V19.3334H8V22ZM13.3333 22H10.6667V19.3334H13.3333V22ZM18.6667 22H16V19.3334H18.6667V22Z" fill="#404040"/>
   </svg>
-  
 
   return (
     <div className={styles.earnings__container}>
@@ -104,7 +106,7 @@ const Ordenes = ({ idLinker }: Props) => {
             <h3 className={styles.results__title}>{order.firstName}</h3>
             <p className={styles.results__text}><span className={styles.results__span}> Orden ID: </span> {order.orderId}</p>
             <p className={styles.results__text}><span className={styles.results__span}>Fecha de compra: </span> {new Date(order.orderDate).toLocaleDateString('es-ES', options)}</p>
-            <p className={styles.results__text}><span className={styles.results__span}>Total de la compra: $ </span>{order.orderValue.toLocaleString("es-ES", { useGrouping: true, maximumFractionDigits: 2, minimumFractionDigits: 2, style: 'currency', currency: 'EUR' })}</p>
+            <p className={styles.results__text}><span className={styles.results__span}>Total de la compra: </span>{numbro((order?.orderValue)/100).format('$0,0')}</p>
           </div>
         ))}
       </div>
